0949ed9a279673380fd293f4a8d75fa36053101c
Naman Khater <naman22khater@gmail.com>
2024-04-07 12:02:55 +0530

    dynamic token decryption

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -8,8 +8,8 @@ const authenticateUserMiddleware = async (req, res, next) => {
         if (!token) {
             return errorResponse(res, { error: 'Authentication error', message: "Please authenticate" }, 401)
         }
-        if (jwt.verify(token)) {
-            const decoded = jwt.decode(token)
+        if (jwt.verify(token, process.env['AES_GCM_ENCRYPTION_KEY_' + process.env.NODE_ENV.toUpperCase()], process.env['JWT_TOKEN_SECRET_' + process.env.NODE_ENV.toUpperCase()], process.env['AES_GCM_ENCRYPTION_IV_' + process.env.NODE_ENV.toUpperCase()])) {
+            const decoded = jwt.decode(token, process.env['AES_GCM_ENCRYPTION_KEY_' + process.env.NODE_ENV.toUpperCase()], process.env['JWT_TOKEN_SECRET_' + process.env.NODE_ENV.toUpperCase()], process.env['AES_GCM_ENCRYPTION_IV_' + process.env.NODE_ENV.toUpperCase()])
             const user = await User.findOne({ email: decoded.email })
             if (!user) {
                 return errorResponse(res, { error: 'Authentication error', message: "Developer not found" }, 404)
--- services/SystemConfiguration/index.js
+++ services/SystemConfiguration/index.js
@@ -6,6 +6,10 @@ async function createSystemConfiguration (req, res) {
         if (!req.body.name || !req.body.key || !req.body.value || !req.body.env) {
             return errorResponse(res, 'Please provide all the required fields', 400);
         }
+        const systemConfigurationExists = await SystemConfiguration.findOne({ key: req.body.key });
+        if (systemConfigurationExists) {
+            return errorResponse(res, 'System Configuration already exists', 400);
+        }
         const systemConfiguration = new SystemConfiguration(req.body);
         await systemConfiguration.save();
         return successResponse(res, systemConfiguration, 'System Configuration created successfully');
